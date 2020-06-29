209ac19e2cd637604be5f780ae5b6b900f9514a0
Kazi Amit Hasan <kaziamithasan89@gmail.com>
2020-06-29 10:24:10 +0600

    data preparation for the model added(shuffling and normalizing)

--- ML with JS/FirstProject/script.js
+++ ML with JS/FirstProject/script.js
@@ -51,8 +51,42 @@ function createModel(){
     const model = tf.sequential();
     /* adding layers */
     model.add(tf.layers.dense({inputShape: [1],units:10, useBias:true}));
+    //model.add(tf.layers.dense({inputShape: [1],units:0, useBias:true}));
 
     model.add(tf.layers.dense({units:1,useBias: true}));
 
     return model;
 }
+// Prepare the model 
+
+function convertToTensor(data){
+    return tf.tidy(() =>{
+        // Shuffle the data
+        tf.util.shuffle(data);
+        // Convert data into tensors
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg);
+        const inputTensor = tf.tensor2d(inputs,[inputs.length,1]);
+        const labelsTensor = tf.tensor2d(labels, [labels.length,1]);
+
+        // normalize the data to range 0 - 1 using min max scaling
+
+
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelmax = labelsTensor.max();
+        const labelmin = labelsTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax,sub(inputMin));
+        const normalizedLabels = inputTensor.sub(labelmin).div(labelmax,sub(labelmin));
+
+
+        return {
+            inputs : normalizedInputs,
+            labels: normalizedLabels,
+
+            inputMax,inputMin,labelmax,labelmin,
+        }
+    });
+}
+
