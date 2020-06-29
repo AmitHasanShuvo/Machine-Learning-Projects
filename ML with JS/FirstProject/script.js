57f8fca0d904def656085d506408ce2d07a51fec
Kazi Amit Hasan <kaziamithasan89@gmail.com>
2020-06-29 11:39:42 +0600

    Full project done

--- ML with JS/FirstProject/script.js
+++ ML with JS/FirstProject/script.js
@@ -26,7 +26,7 @@ async function run() {
         x: d.horsepower,
         y: d.mpg,
     }));
-/*This tfvis is helping to visulize the data */
+    /*This tfvis is helping to visulize the data */
     tfvis.render.scatterplot({
         name: 'Horsepower vs MPG'
     }, {
@@ -39,35 +39,59 @@ async function run() {
 
     // Creating the model
     const model = createModel();
-    tfvis.show.modelSummary({name: 'Model Summary'},model);
+    tfvis.show.modelSummary({
+        name: 'Model Summary'
+    }, model);
+
+    //convert the data to a form ,so we can train them
+    const tensorData = convertToTensor(data);
+    const {inputs,labels} = tensorData;
+
+    // Train the model
+
+    await trainModel(model,inputs,labels);
+    console.log('Done training');
+
+    testModel(model,data,tensorData)
+
 }
 document.addEventListener('DOMContentLoaded', run);
 
 
 /* Now we will defin the model architecture */
 
-function createModel(){
+function createModel() {
     /* dec the model */
     const model = tf.sequential();
     /* adding layers */
-    model.add(tf.layers.dense({inputShape: [1],units:10, useBias:true}));
+    model.add(tf.layers.dense({
+        inputShape: [1],
+        units: 50,
+        useBias: true
+    }));
     //model.add(tf.layers.dense({inputShape: [1],units:0, useBias:true}));
 
-    model.add(tf.layers.dense({units:1,useBias: true}));
+    model.add(tf.layers.dense({
+        units: 1,
+        useBias: true
+    }));
 
     return model;
 }
 // Prepare the model 
 
-function convertToTensor(data){
-    return tf.tidy(() =>{
+function convertToTensor(data) {
+    return tf.tidy(() => {
         // Shuffle the data
         tf.util.shuffle(data);
         // Convert data into tensors
+        // making two arrays
         const inputs = data.map(d => d.horsepower)
         const labels = data.map(d => d.mpg);
-        const inputTensor = tf.tensor2d(inputs,[inputs.length,1]);
-        const labelsTensor = tf.tensor2d(labels, [labels.length,1]);
+
+        // converting arrrays into 2d tensors
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelsTensor = tf.tensor2d(labels, [labels.length, 1]);
 
         // normalize the data to range 0 - 1 using min max scaling
 
@@ -77,16 +101,90 @@ function convertToTensor(data){
         const labelmax = labelsTensor.max();
         const labelmin = labelsTensor.min();
 
-        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax,sub(inputMin));
-        const normalizedLabels = inputTensor.sub(labelmin).div(labelmax,sub(labelmin));
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax. sub(inputMin));
+        const normalizedLabels = labelsTensor.sub(labelmin).div(labelmax. sub(labelmin));
 
 
         return {
-            inputs : normalizedInputs,
+            inputs: normalizedInputs,
             labels: normalizedLabels,
 
-            inputMax,inputMin,labelmax,labelmin,
+            inputMax,
+            inputMin,
+            labelmax,
+            labelmin,
         }
     });
 }
 
+// Train the model
+
+async function trainModel(model, inputs, labels) {
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+    });
+
+    const batchsize = 64;
+    const epochs = 100;
+
+    return await model.fit(inputs, labels,{
+        batchsize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks({
+                name: 'Train Performace'
+            },
+            ['loss', 'mse'], {
+                height: 200,
+                callbacks: ['onEpochEnd']
+            }
+        )
+    });
+}
+
+//Make predictons
+function testModel(model, inputData, normalizationData) {
+    const {inputMax, inputMin, labelmin, labelmax} = normalizationData;  
+    
+    // Generate predictions for a uniform range of numbers between 0 and 1;
+    // We un-normalize the data by doing the inverse of the min-max scaling 
+    // that we did earlier.
+    const [xs, preds] = tf.tidy(() => {
+      
+      const xs = tf.linspace(0, 1, 100);      
+      const preds = model.predict(xs.reshape([100, 1]));      
+      
+      const unNormXs = xs
+        .mul(inputMax.sub(inputMin))
+        .add(inputMin);
+      
+      const unNormPreds = preds
+        .mul(labelmax.sub(labelmin))
+        .add(labelmin);
+      
+      // Un-normalize the data
+      return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+    
+   
+    const predictedPoints = Array.from(xs).map((val, i) => {
+      return {x: val, y: preds[i]}
+    });
+    
+    const originalPoints = inputData.map(d => ({
+      x: d.horsepower, y: d.mpg,
+    }));
+    
+    
+    tfvis.render.scatterplot(
+      {name: 'Model Predictions vs Original Data'}, 
+      {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+  }
